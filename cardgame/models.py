0b1b1b61a1ddabbaa1ce1088cfdef168d5b76711
simeunseo <rkdcjfehowl@naver.com>
2022-07-22 02:57:31 +0900

    Feat : defender의 카드 선택에 따른 victory_user 할당 및 무승부 처리

--- cardgame/models.py
+++ cardgame/models.py
@@ -10,8 +10,9 @@ STATUS_CHOICE = {('proceed', 'proceed'),('end', 'end')}
 class Game(models.Model):
   attacker = models.ForeignKey(User, on_delete=models.CASCADE, related_name="attacker")
   defender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="defender", null=True)
-  attack_card = models.CharField(max_length=50, null=True)
-  defend_card = models.CharField(max_length=50, null=True)
+  attack_card = models.IntegerField(null=True)
+  defend_card = models.IntegerField(null=True, blank=True)
   game_mode = models.CharField(max_length=20, choices=MODE_CHOICE)
   game_status = models.CharField(max_length=20, choices=STATUS_CHOICE)
   victory_user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+  tie_flag = models.IntegerField(default=0)
--- cardgame/views.py
+++ cardgame/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.urls import is_valid_path
 from django.views import View
 from . import forms
-from .models import User
+from .models import User, Game
 from django.contrib.auth import authenticate, login, logout
 
 class LoginView(View):
@@ -55,20 +55,30 @@ def log_out(request):
     logout(request)
     return redirect("cardgame:main")
 
-def defend(request):
+def defend(request, pk):
+    game = Game.objects.get(pk = pk) 
+    
     if request.method == "POST":
-        form = forms.DefendForm(request.POST)
+        form = forms.DefendForm(request.POST, instance=game)
         
         if form.is_valid():
-            defend = form.save()
-            defend.user = request.user
-            defend.save()
+            if game.attack_card == game.defend_card : #공격카드와 방어카드가 같은 수라면
+                game.tie_flag = 1 #무승부 표시
+            else :
+                if game.game_mode == 'big_num' : #큰 수가 이기는 게임이라면
+                    #공격 카드가 큰 수이면 공격자가 이기고, 아니면 방어자가 이긴다
+                    game.victory_user = game.attacker if game.attack_card > game.defend_card else game.defender
+                else : #작은 수가 이기는 게임이라면
+                    #공격 카드가 작은 수이면 공격자가 이기고, 아니면 방어자가 이긴다
+                    game.victory_user = game.attacker if game.attack_card < game.defend_card else game.defender
+            form.save()
             return redirect('/')
         else :
             return redirect('/')
     else :
         form = forms.DefendForm()
         context = {
-            'form' : form
+            'form' : form,
+            'game' : game
         }
         return render(request, 'cardgame/defend.html', context=context)
