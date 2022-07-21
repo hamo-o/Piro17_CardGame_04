63f7d98a6908770a19780f26b6887d5d8734bd4b
doongdang <whtjrgus0428@gmail.com>
2022-07-21 20:30:28 +0900

    add main & sign_up & log_out & LoginView to views

--- cardgame/urls.py
+++ cardgame/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = "cardgame"
 
 urlpatterns = [
-
+    path("", views.main, name="main"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.log_out, name="log_out"),
+    path("signup/", views.sign_up, name="sign_up"),
 ]
